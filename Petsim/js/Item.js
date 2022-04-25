400d754d6860537716a3de260faab10fed4fb5bb
jordans999 <104352211+jordans999@users.noreply.github.com>
2022-04-25 04:21:54 -0500

    Delete Petsim directory

--- Petsim/js/Item.js
+++
@@ -1,9 +0,0 @@
-class Item extends Obj{
-     constructor(x, y, w, h, type, color){
-          super(x,y,w,h);
-          this.type = type;
-          this.color = color;
-     }
-
-     
-}
--- Petsim/js/Water.js
+++
@@ -1,7 +0,0 @@
-class Water extends Food{
-     constructor(x,y){
-          super(x,y);
-          this.type = "water";
-          this.color = rgb(2, 194, 167);
-     }
-}
